86d07fdc8f8945a58182c0e8d75a34091bc90db7
amirhussein soori <amirhusseinsoori75@gmail.com>
2021-04-11 11:48:32 +0430

    create searchQuery for Task Fragment

--- app/src/main/java/com/amirhusseinsoori/mvvmRoomCleanArc/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/amirhusseinsoori/mvvmRoomCleanArc/ui/tasks/TasksFragment.kt
@@ -1,13 +1,18 @@
 package com.amirhusseinsoori.mvvmRoomCleanArc.ui.tasks
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.amirhusseinsoori.mvvmRoomCleanArc.R
 import com.amirhusseinsoori.mvvmRoomCleanArc.databinding.FragmentTasksBinding
+import com.amirhusseinsoori.mvvmRoomCleanArc.ui.util.onQueryTextChanged
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
@@ -39,5 +44,40 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
             }
         }
 
+        setHasOptionsMenu(true)
+
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_fragment_tasks, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.onQueryTextChanged {
+            viewModel.searchQuery.value = it
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_sort_by_name -> {
+
+                true
+            }
+            R.id.action_sort_by_date_created -> {
+
+                true
+            }
+            R.id.action_hide_completed_tasks -> {
+                item.isChecked = !item.isChecked
+
+                true
+            }
+            R.id.action_delete_all_completed_tasks -> {
+
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/amirhusseinsoori/mvvmRoomCleanArc/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/amirhusseinsoori/mvvmRoomCleanArc/ui/tasks/TasksViewModel.kt
@@ -4,12 +4,18 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.amirhusseinsoori.mvvmRoomCleanArc.data.TaskDao
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 import javax.inject.Inject
 
 @HiltViewModel
 class TasksViewModel @Inject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+    val searchQuery = MutableStateFlow("")
 
-    val tasks = taskDao.getTasks()
+    private val tasksFlow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+    val tasks = tasksFlow
 }
